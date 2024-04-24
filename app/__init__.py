5b855888936774f8e3e3d751b0a9ac5b28c451a9
thisiscaro <carolinemmen@gmail.com>
2024-04-24 17:20:49 -0500

    fix small issue

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from .routes.games_routes import games_routes
 
-
 app = Flask(__name__)
 
-app.config["SECRET_KEY"] = "your-secret-key"
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
 db = SQLAlchemy(app)
 
-
-app.register_blueprint(games_routes, url_prefix="/games")
+app.register_blueprint(games_routes, url_prefix="/game")
