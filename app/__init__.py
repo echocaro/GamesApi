0f3cc57f1ab6175a0cdaa261cc981b2c3f38fab8
thisiscaro <carolinemmen@gmail.com>
2024-04-25 18:04:20 -0500

    more setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 import os
 from .routes.games_routes import games_routes
+from .models.db import db
 
 app = Flask(__name__)
-
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 
-db = SQLAlchemy(app)
+db.init_app(app)
+Migrate(app, db)
 
 app.register_blueprint(games_routes, url_prefix="/game")
